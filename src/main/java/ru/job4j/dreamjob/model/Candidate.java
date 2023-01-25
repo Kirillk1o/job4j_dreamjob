2661503711bb1de706b67583a3f3fb503d84fd38
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2023-01-25 22:38:43 +0300

    5. Создание, просмотр, редактирование и удаление кандидатов [#504858]

--- src/main/java/ru/job4j/dreamjob/model/Candidate.java
+++ src/main/java/ru/job4j/dreamjob/model/Candidate.java
@@ -10,13 +10,16 @@ public class Candidate {
     private int id;
     private String name;
     private String description;
-    private LocalDateTime creationDate;
+    private LocalDateTime creationDate = LocalDateTime.now();
 
     public Candidate(int id, String name, String description, LocalDateTime creationDate) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.creationDate = creationDate;
+    }
+
+    public Candidate() {
     }
 
     public int getId() {
@@ -31,12 +34,24 @@ public class Candidate {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getDescription() {
         return description;
     }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public LocalDateTime getCreationDate() {
         return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
     }
 
     @Override
