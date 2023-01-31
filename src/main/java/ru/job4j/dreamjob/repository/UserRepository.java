f3e25b78deac35f7fa15052d195b95fa5dc735c3
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2023-01-31 22:40:56 +0300

    4. Многопоточность в базе данных [#504860]

--- src/main/java/ru/job4j/dreamjob/repository/UserRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/UserRepository.java
@@ -5,6 +5,8 @@ import ru.job4j.dreamjob.model.User;
 import java.util.Optional;
 
 public interface UserRepository {
+
     Optional<User> save(User user);
+
     Optional<User> findByEmailAndPassword(String email, String password);
 }
